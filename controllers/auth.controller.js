1a28fa2a9538c57da03be95d31aed8a7a3fb136b
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-15 19:14:47 +1000

    add register schema to data base with unique

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,30 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
 
 export const register =  async(req,res)=>{
-   const{username,email,password}= req.body
+   
+   const{username,email,password}= req.body;
    // hash the password
+   try{
+
+  
    const hashedPassword = await bcrypt.hash(password,10)
    console.log(hashedPassword)
 
+   const newUser= await prisma.user.create({
+      data:{
+         username,
+         email,
+         password : hashedPassword,
+
+      }
+   }); 
+   console.log(newUser)
+   res.status(201).json({mesaage:'user created successfully'})
+}catch(err){
+   console.log(err)
+   res.status(500).json({message:'something went wrong'})
+}
 }
 export const login = (req,res)=>{
 
