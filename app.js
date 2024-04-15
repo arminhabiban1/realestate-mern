c358d8f95b6c90c7dc9146ca7c5ce59c7f0d8b23
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-15 21:06:59 +1000

    setup token for env

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import postRouter from './/routes/post.route.js'
 import authRouter from './/routes/auth.route.js'
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
  app.use('/api/posts',postRouter)
  app.use('/api/auth',authRouter)
 app.listen(3000,()=>{
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import prisma from "../lib/prisma.js"
 
 export const register =  async(req,res)=>{
@@ -26,7 +27,43 @@ export const register =  async(req,res)=>{
    res.status(500).json({message:'something went wrong'})
 }
 }
-export const login = (req,res)=>{
+export const login = async (req,res)=>{
+    const{username,password} =req.body;
+    
+    try{
+      //check if the user exists
+      const user= await prisma.user.findUnique({where:{username}})
+      if(!user)
+        return  res.status(401).json({message:'invalid credentials'})
+        //check if the password is correct
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid)
+return res.status(401).json({message:"invalid credential"})
+
+      
+//generate cookie token and send to the user
+const age = 1000*60*60*24*7
+const token = jwt.sign({
+   id:user.id
+},process.env.JWT_SERCRET_KEY,{expiresIn:age})
+
+res.cookie("token",token,{
+httpOnly: true,
+maxAge: age
+}).status(200).json({message:"success full login"})
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({message:'something went wrong'})
+    }
+
+
+
+    
+
+
+    
 
 }
 export const logout = (req,res)=>{
